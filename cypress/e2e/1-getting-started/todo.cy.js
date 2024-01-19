83368178e63530a7fc5cdf784404c2f43c2a5607
Huzaifa Tariq <122553707+huzaifa898@users.noreply.github.com>
2024-01-19 16:00:18 +0500

    crud opearrtion test case

--- cypress/e2e/1-getting-started/todo.cy.js
+++ cypress/e2e/1-getting-started/todo.cy.js
@@ -183,3 +183,57 @@ describe('Add Books Section', () => {
     cy.get('.error-message').should('exist');
   });
 });
+
+// crud opeartion test case
+// cypress/integration/books_crud_spec.js
+
+describe('Books CRUD Operations', () => {
+  beforeEach(() => {
+    // Assuming your server is running on http://localhost:1000
+    cy.request('POST', 'http://localhost:1000/api/v1/add', {
+      bookname: 'Test Book',
+      author: 'John Doe',
+      description: 'A great book for testing',
+      image: 'https://example.com/book-image.jpg',
+      price: 19.99,
+    }).as('addedBook');
+  });
+
+  it('should add a new book', () => {
+    cy.visit('/books');
+    // Add assertions to check if the newly added book is displayed
+    cy.get('.book-item').should('have.length.greaterThan', 0);
+  });
+
+  it('should update an existing book', () => {
+    // Assuming you have a book ID from the previously added book
+    cy.request('GET', 'http://localhost:1000/api/v1/getBooks').then((response) => {
+      const firstBookId = response.body.books[0]._id;
+
+      cy.request('PUT', `http://localhost:1000/api/v1/updateBook/${firstBookId}`, {
+        bookname: 'Updated Book Name',
+        description: 'Updated book description',
+        author: 'Updated Author',
+        image: 'https://example.com/updated-image.jpg',
+        price: 29.99,
+      });
+
+      // Add assertions to check if the book is updated
+      cy.visit(`/books/${firstBookId}`);
+      cy.get('.book-details').should('contain', 'Updated Book Name');
+    });
+  });
+
+  it('should delete an existing book', () => {
+    // Assuming you have a book ID from the previously added book
+    cy.request('GET', 'http://localhost:1000/api/v1/getBooks').then((response) => {
+      const firstBookId = response.body.books[0]._id;
+
+      cy.request('DELETE', `http://localhost:1000/api/v1/deleteBook/${firstBookId}`);
+
+      // Add assertions to check if the book is deleted
+      cy.visit('/books');
+      cy.get('.book-item').should('have.length', 0);
+    });
+  });
+});
